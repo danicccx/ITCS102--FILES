954c1b233d136a3278b4e1793eeb28be54a2aa1b
danicccx <danieule55@gmail.com>
2025-08-13 15:02:02 +0800

    Add files via upload

---
+++ code_challenge.py
@@ -0,0 +1,2 @@
+name = input("type your name -->")
+print("\t\t\t\t*\n\n\n\t\t\t*\t\t*\n\n\n\t\t*\t\t\t\t*\n\n\n\t*\t\tHi\t\t\t\t*\t\t\t\t\t\t\n\n\n*\t\t\t\t",name,"\t\t\t\t*\n\n\n\t*\t\t\t\t\t\t*\n\n\n\t\t*\t\t\t\t*\n\n\n\t\t\t*\t\t*\n\n\t\t\t\t*")

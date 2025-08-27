875c39fe38153553b6d8543e29071f9a66c3f9c8
danicccx <danieule55@gmail.com>
2025-08-27 15:08:39 +0800

    Add files via upload

---
+++ activity_10.py
@@ -0,0 +1,9 @@
+username = "chasing_blue"
+password = "heartheartblue"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password): print("ACCESS GRANTED")
+else: print("ACCESS DENIED")
+

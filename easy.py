0e9b5b292a296f458444596f76dcc0f74ca0768f
danicccx <danieule55@gmail.com>
2025-09-10 14:48:00 +0800

    Add files via upload

---
+++ easy.py
@@ -0,0 +1,45 @@
+message = ""
+person1 = input("Please input something --> ")
+message += person1
+
+person2 = input("Please input something --> ")
+message += person2
+
+person3 = input("Please input something --> ")
+message += person3
+
+person4 = input("Please input something --> ")
+message += person4
+
+person5 = input("Please input something --> ")
+message += person5
+
+person6 = input("Please input something --> ")
+message += person6
+
+person7 = input("Please input something --> ")
+message += person7
+
+person8 = input("Please input something --> ")
+message += person8
+
+person9 = input("Please input something --> ")
+message += person9
+
+person10 = input("Please input something --> ")
+message += person10
+
+person11 = input("Please input something --> ")
+message += person11
+
+person12 = input("Please input something --> ")
+message += person12
+
+person13 = input("Please input something --> ")
+message += person13
+
+person14 = input("Please input something --> ")
+message += person14
+
+person15 = input("Please input something --> ")
+message += person15

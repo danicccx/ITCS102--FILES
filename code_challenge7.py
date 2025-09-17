8d36194caaa937ac70b8d3793c8f4a407d09d3b7
danicccx <danieule55@gmail.com>
2025-09-17 12:02:53 +0800

    Create code_challenge7.py

---
+++ code_challenge7.py
@@ -0,0 +1,9 @@
+print("WELCOME TO ⏳COUNTDOWN TIMER STIMULATOR⏳")
+countdown = eval(input("Enter the starting number for countdown:"))
+print("Countdown started:")
+for c in range(countdown 0, -1):
+  print(c)
+
+print("LIFT-OFF!)
+      
+      

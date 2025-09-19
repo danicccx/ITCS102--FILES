065825b2fa11b4f80c61eb2cf854a26db865fa00
danicccx <danieule55@gmail.com>
2025-09-19 23:45:03 +0800

    Create code_challenge9.py

---
+++ code_challenge9.py
@@ -0,0 +1,7 @@
+print("🦜 PARROT SIMULATOR- THE CHAMBER!")
+
+parrot = input("What you want your parrot to say?")
+repeat= eval(input("How many times should the parrot squawk?"))
+
+for p in range(repeat):
+  print("🦜 SQUAWK!", parrot")

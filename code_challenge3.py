f67336a7a53f483d1079b32ffd7e0f195dd8e766
danicccx <danieule55@gmail.com>
2025-09-20 20:18:50 +0800

    Create code_challenge3.py

---
+++ code_challenge3.py
@@ -0,0 +1,13 @@
+name = input("What is your name? --->")
+fare = eval(input("Fare fee ---> "))
+isStudent = input("Are you currently a student (yes/no): ")
+
+if isStudent == 'yes':
+    discount = fare * 0.2
+    new_fare = fare - discount
+    print("Hi ", name)
+    print("Your Discount is ", discount)
+    print("Your new fare is ", new_fare)
+    
+else:
+    print("Sorry ", name, "You are not eligible for a student discount")

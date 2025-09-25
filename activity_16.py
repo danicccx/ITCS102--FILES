ae822f472583c1c79e369d142ac6017084c8ee0f
danicccx <danieule55@gmail.com>
2025-09-25 18:01:31 +0800

    Create activity_16.py

---
+++ activity_16.py
@@ -0,0 +1,11 @@
+# for loop(1)
+
+print("I love black forest cake", end=" ")
+print("Dirty Match", end=" ")
+print("Good bye!", end=" ")
+print( )
+print( )
+
+#for loop(2)
+for d in range(1,11,1):
+    print(d, end= " ")

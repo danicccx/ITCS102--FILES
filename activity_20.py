858f6fd411e35fb455cd8b9319c45909636ba15d
danicccx <danieule55@gmail.com>
2025-10-16 12:02:34 +0800

    Create activity_20.py

---
+++ activity_20.py
@@ -0,0 +1,18 @@
+print("\t\t *", end=" ")
+for a in range (1, 11, 1):
+        for b in range (10, a, -1):
+            print(" ", end = " ")
+        for c in range (1, a, 1):
+            print("*", end = " ")
+        for d in range (1, a, 1):
+                print("*", end = " ")
+        print ( )
+        
+for e in range(1,11,1):
+        for f in range(1,a,1):
+            print(" ", end = " ")
+        for g in range(10,a,-1):
+            print("*", end = " ")
+        for h in range(10,a,-1):
+            print("*", end = " ")
+        print( )
